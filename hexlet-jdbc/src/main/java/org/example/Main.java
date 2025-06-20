e9ff0fa72f465ac43ad72d296b76c5755812d15c
brein594 <brein594@gmail.com>
2025-06-20 17:13:41 +0300

    add try-with-resorces java.lang.AutoCloseable

--- hexlet-jdbc/src/main/java/org/example/Main.java
+++ hexlet-jdbc/src/main/java/org/example/Main.java
@@ -4,26 +4,26 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
 
-        var sql12 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql12);
-        statement2.close();
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }//statement.close();
 
-        var sql13 = "SELECT * FROM users";
-        var statement3 = conn.createStatement();
-        var resultSet = statement3.executeQuery(sql13);
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
-        }
-        statement3.close();
-        conn.close();
+            var sql12 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql12);
+            } //statement2.close();
 
+            var sql13 = "SELECT * FROM users";
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql13);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }//statement3.close();
+        } //conn.close();
     }
 }
