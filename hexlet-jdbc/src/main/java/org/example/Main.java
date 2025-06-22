12e3f2fbf3b36511f43874f781ba63011dff9688
brein594 <brein594@gmail.com>
2025-06-22 12:37:43 +0300

    update to prepareStatement

--- hexlet-jdbc/src/main/java/org/example/Main.java
+++ hexlet-jdbc/src/main/java/org/example/Main.java
@@ -9,12 +9,21 @@ public class Main {
             var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
             try (var statement = conn.createStatement()) {
                 statement.execute(sql);
-            }//statement.close();
+            }//statement.close(); //переход на try
 
-            var sql12 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql12);
-            } //statement2.close();
+            //var sql12 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456')"; //переходн на плейсхолдеры
+            var sql12 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql12)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "3333333");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Maria");
+                preparedStatement.setString(2, "4444444");
+                preparedStatement.executeUpdate();
+            //try var statement2 = conn.createStatement()) {
+               // statement2.executeUpdate(sql12);
+            } //statement2.close();  //переход на try
 
             var sql13 = "SELECT * FROM users";
             try (var statement3 = conn.createStatement()) {
@@ -23,7 +32,7 @@ public class Main {
                     System.out.println(resultSet.getString("username"));
                     System.out.println(resultSet.getString("phone"));
                 }
-            }//statement3.close();
+            }//statement3.close(); //переход на try
         } //conn.close();
     }
 }
